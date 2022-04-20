df0b78611e8828ef3f18a9e22986d461dce903a8
Daniil Nikitas <daniil_nikitas@epam.com>
2022-04-20 20:46:15 +0600

    added priority queue

---
+++ src/algoritms-1/PriorityQueue.ts
@@ -0,0 +1,61 @@
+interface QueueItem {
+  item: number
+  priority: number
+}
+
+class PriorityQueue {
+  queue: QueueItem[] = []
+
+  get size() {
+    return this.queue.length
+  }
+
+  get isEmpty() {
+    return this.size === 0
+  }
+
+  enqueue(item: number, priority: number) {
+    const element: QueueItem = { item, priority }
+    if (
+      this.isEmpty ||
+      element.priority >= this.queue[this.size - 1].priority
+    ) {
+      this.queue.push(element)
+    } else {
+      for (const index in this.queue) {
+        if (element.priority < this.queue[index].priority) {
+          this.queue.splice(+index, 0, element)
+          break
+        }
+      }
+    }
+  }
+
+  dequeue() {
+    this.isEmpty ? null : this.queue.shift().item
+  }
+
+  print() {
+    console.log(this.queue)
+  }
+
+  toString() {
+    this.queue.forEach(el => {
+      console.log('Job Item', el.item, 'Job Priority', el.priority)
+    })
+  }
+  getRandomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+}
+
+const pq = new PriorityQueue()
+
+for (let i = 0; i < 5; i++) {
+  const randomNumber = pq.getRandomNumber(0, 5)
+  pq.enqueue(randomNumber, randomNumber)
+}
+
+//pq.dequeue()
+pq.toString()
+pq.print()
